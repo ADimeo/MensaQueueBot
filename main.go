b6ae9b15efd89baff2719070eed35cad029f0cfb
Antonio Dimeo <antoniocarlos.dimeo@gmail.com>
2022-05-09 20:46:07 +0200

    Add relative reporting of queue measurement time for better UX

--- main.go
+++ main.go
@@ -226,16 +226,19 @@ func getLocalLocation() *time.Location {
 
 /*
    Sends a message to the specified user, depending on when the last reported queue length was;
-   - For reported lengths within the last 5 minute
+   - For reported lengths within the last 5 minutes
+   - For reported lengths within the last 59 minutes
    - For reported lengths on the same day
    - For no reported length on the same day
 */
 func sendQueueLengthReport(chatID int, timeOfReport int, reportedQueueLength string) {
 	baseMessageReportAvailable := "Current length of mensa queue is %s"
+	baseMessageRelativeReportAvailable := "%d minutes ago the length was %s"
 	baseMessageNoRecentReportAvailable := "No recent report, but today at %s the length was %s"
 	baseMessageNoReportAvailable := "No queue length reported today."
 
 	acceptableDeltaSinceLastReport, _ := time.ParseDuration("5m")
+	timeDeltaForRelativeTimeSinceLastReport, _ := time.ParseDuration("59m")
 
 	timestampNow := time.Now()
 	timestampThen := time.Unix(int64(timeOfReport), 0)
@@ -251,6 +254,8 @@ func sendQueueLengthReport(chatID int, timeOfReport int, reportedQueueLength str
 	timeSinceLastReport := timestampNow.Sub(timestampThen)
 	if timeSinceLastReport <= acceptableDeltaSinceLastReport {
 		err = SendMessage(chatID, fmt.Sprintf(baseMessageReportAvailable, reportedQueueLength))
+	} else if timeSinceLastReport <= timeDeltaForRelativeTimeSinceLastReport {
+		err = SendMessage(chatID, fmt.Sprintf(baseMessageRelativeReportAvailable, int(timeSinceLastReport.Minutes()), reportedQueueLength))
 	} else if timestampNow.YearDay() == timestampThen.YearDay() {
 		err = SendMessage(chatID, fmt.Sprintf(baseMessageNoRecentReportAvailable, timestampThen.Format("15:04"), reportedQueueLength))
 	} else {
